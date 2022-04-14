a0648429c5760aa14e768aa8ee4bcc80ef3c8bf4
JnBanks <joku.banks@gmail.com>
2022-04-14 14:07:18 -0700

    basic files, all linked, started the html

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("test")
+//3fb3c92051c198c34c422dbb5e5acac5
+//above is my weatherapp API Key
+var weatherApiKey = "3fb3c92051c198c34c422dbb5e5acac5";
+var city;
+//Use the below format for calling cities by name and state (because there are many cities by the same name across the US)
+//https://api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
