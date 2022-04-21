d4d00c84b1c080d0f2defb026bfc5f7a297f94b5
JnBanks <joku.banks@gmail.com>
2022-04-21 08:12:42 -0700

    updated all forecast information

--- script.js
+++ script.js
@@ -1,87 +1,135 @@
 // That first acceptance criteria is big. let's break it down.
-// build the html and css to support the page
-// how to fetch data
-//  successfully console log an array from open weather api
-//  Place the date in the current day box
-//  place the temp in the current day box
-//    convert that temp to Fahrenheit before displaying it. this was done by adjusting the api call, not by trying to add a function that converts kelvin to fahrenheit lol
-//  place the wind in the current day box  
+//  build the html and css to support the page and display the weather data
+//    how to fetch data
+//    successfully console log an array from open weather api
+//      complete the current day box
+//        Place the date in the current day box
+//        place the temp in the current day box
+//         convert that temp to Fahrenheit before displaying it. this was done by adjusting the api call, not by trying to add a function that converts kelvin to fahrenheit lol
+//        place the wind in the current day box, then the humidity, then the uv index
+//      complete the five forecast day boxes
+//        
+//      add the city name to the search history column beneath the search button     
 
 //declare variables:
 const searchBtn = document.querySelector('#search-btn');
 const cityDate = document.querySelector('#city-date');
-const todaysDate = moment().format('MMM Do, YYYY');
+const todaysDate = moment().format('(M/D/YYYY)');
 const todayTemp = document.querySelector('#temp');
 const todayWind = document.querySelector('#wind');
 const todayHumidity = document.querySelector('#humidity');
 const todayUv = document.querySelector('#uv');
+//forecast variables
+//  dates
+const f1date = document.querySelector('#forecast1 .date');
+const f2date = document.querySelector('#forecast2 .date');
+const f3date = document.querySelector('#forecast3 .date');
+const f4date = document.querySelector('#forecast4 .date');
+const f5date = document.querySelector('#forecast5 .date');
+//  icons
+const f1icon = document.querySelector('#forecast1 .icon');
+const f2icon = document.querySelector('#forecast2 .icon');
+const f3icon = document.querySelector('#forecast3 .icon');
+const f4icon = document.querySelector('#forecast4 .icon');
+const f5icon = document.querySelector('#forecast5 .icon');
+//  temp
+const f1temp = document.querySelector('#forecast1 .temp');
+const f2temp = document.querySelector('#forecast2 .temp');
+const f3temp = document.querySelector('#forecast3 .temp');
+const f4temp = document.querySelector('#forecast4 .temp');
+const f5temp = document.querySelector('#forecast5 .temp');
+//  wind
+const f1wind = document.querySelector('#forecast1 .wind');
+const f2wind = document.querySelector('#forecast2 .wind');
+const f3wind = document.querySelector('#forecast3 .wind');
+const f4wind = document.querySelector('#forecast4 .wind');
+const f5wind = document.querySelector('#forecast5 .wind');
+//  humidity
+const f1humidity = document.querySelector('#forecast1 .humidity');
+const f2humidity = document.querySelector('#forecast2 .humidity');
+const f3humidity = document.querySelector('#forecast3 .humidity');
+const f4humidity = document.querySelector('#forecast4 .humidity');
+const f5humidity = document.querySelector('#forecast5 .humidity');
+
 
 function fetchCityDeets() {
-    const weatherKey = "3d9956d2d042c8cf613c82d30cf3b4d2";
-    let city = document.querySelector('#city-input').value;
-    //let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + weatherKey;
-    let queryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city +'&units=imperial&limit=1&appid=' + weatherKey;
+  const weatherKey = "3d9956d2d042c8cf613c82d30cf3b4d2";
+  let city = document.querySelector('#city-input').value;
+  let queryURLCity = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + weatherKey;
+  let queryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city +'&units=imperial&limit=1&appid=' + weatherKey;
+    fetch(queryURLCity)
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data){
+      //console.log(data);
+      cityDate.textContent = data.name + ' ' + todaysDate;
+      todayTemp.textContent = 'Temp: ' + data.main.temp + ' \xB0 F';
+      todayWind.textContent = 'Wind: ' + data.wind.speed + 'mph';
+      todayHumidity.textContent = 'Humidity: ' + data.main.humidity + '%';
+      //the api fetch by city name did not include the uv index, but the latitude-and-longitude-based fetch includes all the info that I need. So what I think needs to be done is a fetch which gets the lat and long, and then use those numbers in a new fetch which can get all the information that I need. 
+    })
     fetch(queryURL)
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
-      //the api fetch by city name did not include the uv index, but the lat an long includes everything. So what I think needs to be done is a fetch which gets the lat and long, and then use those numbers in a new fetch which can get all the information that I need. 
-      console.log(data);
-      console.log(city);
+      //console.log(data);
       const lat = data[0].lat;
       const lon = data[0].lon;
-      fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&appid='+weatherKey)
+      //This fetch below should give me the five day forecast and uv index
+      fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&units=imperial&appid='+weatherKey)
       .then(function(resp) { return resp.json() }) // Convert data to json
       .then(function(data){
         console.log(data);
-        //console.log(city);
+        console.log(city);
+        todayUv.textContent = 'UV Index: ' + data.current.uvi;
+        //forecast dates
+        f1date.textContent = moment(data.daily[1].dt * 1000).format('M/D/YYYY');
+        f2date.textContent = moment(data.daily[2].dt * 1000).format('M/D/YYYY');
+        f3date.textContent = moment(data.daily[3].dt * 1000).format('M/D/YYYY');
+        f4date.textContent = moment(data.daily[4].dt * 1000).format('M/D/YYYY');
+        f5date.textContent = moment(data.daily[5].dt * 1000).format('M/D/YYYY');
+        //Forecast Icons
+        //  Create images inside the div tags
+        const icon1 = document.createElement("img");
+        const icon2 = document.createElement("img");
+        const icon3 = document.createElement("img");
+        const icon4 = document.createElement("img");
+        const icon5 = document.createElement("img");
+        //  Find the icon id in the fetched object
+        const icon1a = data.daily[1].weather[0].icon;
+        const icon2a = data.daily[2].weather[0].icon;
+        const icon3a = data.daily[3].weather[0].icon;
+        const icon4a = data.daily[4].weather[0].icon;
+        const icon5a = data.daily[5].weather[0].icon;
+        //  Insert the icon id into the icon image url
+        icon1.src = 'http://openweathermap.org/img/wn/'+icon1a+'@2x.png';
+        icon2.src = 'http://openweathermap.org/img/wn/'+icon2a+'@2x.png';
+        icon3.src = 'http://openweathermap.org/img/wn/'+icon3a+'@2x.png';
+        icon4.src = 'http://openweathermap.org/img/wn/'+icon4a+'@2x.png';
+        icon5.src = 'http://openweathermap.org/img/wn/'+icon5a+'@2x.png';
+        //  Finally, insert the fetched image into the div to display on the webpage
+        f1icon.appendChild(icon1);
+        f2icon.appendChild(icon2);
+        f3icon.appendChild(icon3);
+        f4icon.appendChild(icon4);
+        f5icon.appendChild(icon5);
+        //Forecast Temp
+        f1temp.textContent = 'Temp: ' + data.daily[1].temp.day + ' \xB0 F';
+        f2temp.textContent = 'Temp: ' + data.daily[2].temp.day + ' \xB0 F';
+        f3temp.textContent = 'Temp: ' + data.daily[3].temp.day + ' \xB0 F';
+        f4temp.textContent = 'Temp: ' + data.daily[4].temp.day + ' \xB0 F';
+        f5temp.textContent = 'Temp: ' + data.daily[5].temp.day + ' \xB0 F';
+        //Forecast Wind
+        f1wind.textContent = 'Wind: ' + data.daily[1].wind_speed + 'mph';
+        f2wind.textContent = 'Wind: ' + data.daily[2].wind_speed + 'mph';
+        f3wind.textContent = 'Wind: ' + data.daily[3].wind_speed + 'mph';
+        f4wind.textContent = 'Wind: ' + data.daily[4].wind_speed + 'mph';
+        f5wind.textContent = 'Wind: ' + data.daily[5].wind_speed + 'mph';
+        //Forecast Humidity
+        f1humidity.textContent = 'Humidity: ' + data.daily[1].humidity + '%';
+        f2humidity.textContent = 'Humidity: ' + data.daily[2].humidity + '%';
+        f3humidity.textContent = 'Humidity: ' + data.daily[3].humidity + '%';
+        f4humidity.textContent = 'Humidity: ' + data.daily[4].humidity + '%';
+        f5humidity.textContent = 'Humidity: ' + data.daily[5].humidity + '%';
       })
-    //cityDate.textContent = data.name + ' ' + todaysDate;
-    //todayTemp.textContent = 'Temp: ' + data.main.temp + ' \xB0 F';
-    //todayWind.textContent = 'Wind: ' + data.wind.speed + 'mph';
-    //todayHumidity.textContent = 'Humidity: ' + data.main.humidity + '%';
-    //todayUv.textContent = 'UV Index: ' + data.
   })
 }
 searchBtn.addEventListener('click', fetchCityDeets);
-
-//fetch(queryURL);
-
-
-// let today = document.querySelector('#city-date');
-// let todayTemp = document.querySelector('#temp');
-// let todayWind = document.querySelector('#wind');
-// let todayHumidity = document.querySelector('#humidity');
-// let todayUv = document.querySelector('#uv');
-
-
-//function displayWeather(){
-// today.innerHTML = 
-// todayTemp.innerHTML = 
-// todayWind.innerHTML =
-// todayHumidity.innerHTML = 
-// todayUv.innerHTML = 
-//}
-
-//I don't know if I need : '', after the object items, or if I can just put a comma after their names. city, vs city: '',
-// const weather = {
-//     city: '',
-//     date,
-//     temperature,
-//     wind,
-//     humidity,
-//     uv,
-// };
-
-//Below is kind of how you can get the city user input and store in local storage
-
-// city = localStorage.getItem("#city-input");
-
-// city-input.textContent = city;
-
-// searchCity.addEventListener("click", function() {
-//     if ()
-//     fetch(queryURL)
-    
-// }
-//Use the below format for calling cities by name
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
